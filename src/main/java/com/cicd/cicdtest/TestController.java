dee6362db2707eebdf500850821dafb58bc639dd
junho100 <bemodesty306@naver.com>
2024-03-01 16:47:43 +0900

    Core: init project

---
+++ src/main/java/com/cicd/cicdtest/TestController.java
@@ -0,0 +1,15 @@
+package com.cicd.cicdtest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class TestController {
+    @GetMapping("/health")
+    public void healthCheck() {
+    }
+}
